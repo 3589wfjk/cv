5d388589a3559bf46b86635233d28baf92882dc5
3589wfjk <106833267+3589wfjk@users.noreply.github.com>
2022-06-04 10:41:58 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+### [Hướng dẫn tạo website tán đổ crush đơn giản và hiệu quả || Part 2](https://youtu.be/e2KYBhCx-uU)
+> Các bạn download source về và làm theo hướng dẫn trong video nhé.
+
+
+![cover picture](/img/totinh2021-part2.jpg)
---
+++ config.js
@@ -0,0 +1,21 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '❤️❤️❤️ Hee loo !❤️❤️❤️',
+    introDesc:  ' Mình có điều này muốn nói với cậu , ngừời tình 100 năm phải xem hết nha ',
+    btnIntro: 'OK',
+    title: 'Anh biết là anh có lỗi nhưng vợ iu sẽ bỏ qua cho anh phải không phải không ? ❤️❤️❤️',
+   
+   
+    desc: 
+    'Em không trả lời hoặc thoát ra, tức là đồng ý đó nha :> ',
+    btnYes: 'Có, tha cho đó đồ ngốc 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Mình biết cậu sẽ bỏ qua đi mà 😊😊',
+    reasonPlaceholder: ' Đáp án ',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'Hết dỗi rồi đó  😊😊',
+    mess: 'Mình biết mà, ngừời gì đâu vừa đẹp vừa dễ thương 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho mình nha 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/100014063614840'
+}
